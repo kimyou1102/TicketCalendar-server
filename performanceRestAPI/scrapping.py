644d7d2a1db4b3aa3cb8f436ed6b148c64176303
kimyou1102 <yoosk5485@naver.com>
2023-01-13 23:15:19 +0900

    fix: 창뜨지 않는 경우 처리

--- performanceRestAPI/scrapping.py
+++ performanceRestAPI/scrapping.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium.webdriver.common.by import By
@@ -45,43 +46,21 @@ def scrapping(driver, datas, musician):
 
             print('NONE_CHECK', date_check == None)
 
-            # if(len(datas) == 0):
-            #     print('첫 추가')
-            #     datas.append(obj)
-
             print('NOT_CHECK', not(obj in datas))
             if(date_check == None):
                 print('추가')
                 datas.append(obj)
 
-            
-            # datas.append(obj)
-
-            # if not(obj in datas):
-            #     datas.append(obj)
 
     time.sleep(5)
 
-def getDatas(musician):
-    url = f'https://twitter.com/search?q=from%3AYES24TicketOpen%20{musician}&src=typed_query&f=top'
-    datas = []
-    driver = webdriver.Chrome('chromedriver.exe')
-    driver.get(url)
-    driver.implicitly_wait(3)
-    # 창 뜰 때까지 기다리기
-    element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-1kihuf0.r-18u37iz.r-1pi2tsx.r-1777fci.r-1pjcn9w.r-xr3zp9.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-14lw9ot.r-1867qdf.r-1jgb5lz.r-pm9dpa.r-1ye8kvj.r-1rnoaur.r-13qz1uu")))
-    time.sleep(3)
-    #나중에 버튼 클릭
-    driver.find_element(By.CSS_SELECTOR, '#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-1kihuf0.r-18u37iz.r-1pi2tsx.r-1777fci.r-1pjcn9w.r-xr3zp9.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-14lw9ot.r-1867qdf.r-1jgb5lz.r-pm9dpa.r-1ye8kvj.r-1rnoaur.r-13qz1uu > div > div.css-1dbjc4n.r-1awozwy.r-16y2uox > div > div.css-1dbjc4n.r-98ikmy.r-hvns9x > div.css-1dbjc4n.r-13qz1uu > div.css-18t94o4.css-1dbjc4n.r-1niwhzg.r-1ets6dv.r-sdzlij.r-1phboty.r-rs99b7.r-1wzrnnt.r-19yznuf.r-64el8z.r-1ny4l3l.r-1dye5f7.r-o7ynqc.r-6416eg.r-lrvibr').click()
-    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@떴다@@@@@@@@@@@@@@@@@@@@@@@@@@')
-
+def load(driver, datas, musician):
     time.sleep(3)
 
     # 스크롤 내리기
     SCROLL_PAUSE_TIME = 2
     # Get scroll height
     last_height = driver.execute_script("return document.body.scrollHeight")         
-
     while True:
         # Scroll down to bottom                                                      
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -90,6 +69,7 @@ def getDatas(musician):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight-50);") 
         # 내리면서 긁어온다(안그러면 사라짐;) 
         scrapping(driver, datas, musician)
+
         time.sleep(SCROLL_PAUSE_TIME)
         # Calculate new scroll height and compare with last scroll height            
         new_height = driver.execute_script("return document.body.scrollHeight")
@@ -98,11 +78,32 @@ def getDatas(musician):
         last_height = new_height
 
     print(len(datas))
-
     for data in datas:
         print(data)
         print('--------------------------------------------------------------------------------------')
 
+
+def getDatas(musician):
+    url = f'https://twitter.com/search?q=from%3AYES24TicketOpen%20{musician}&src=typed_query&f=top'
+    datas = []
+    driver = webdriver.Chrome('chromedriver.exe')
+    driver.get(url)
+    driver.implicitly_wait(3)
+    # 창 뜰 때까지 기다리기
+    try:
+        # ID가 myDynamicElement인 element가 로딩될 때 까지 10초 대기
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-1kihuf0.r-18u37iz.r-1pi2tsx.r-1777fci.r-1pjcn9w.r-xr3zp9.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-14lw9ot.r-1867qdf.r-1jgb5lz.r-pm9dpa.r-1ye8kvj.r-1rnoaur.r-13qz1uu")))
+        print('성공')
+        #나중에 버튼 클릭
+        driver.find_element(By.CSS_SELECTOR, '#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-1kihuf0.r-18u37iz.r-1pi2tsx.r-1777fci.r-1pjcn9w.r-xr3zp9.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-14lw9ot.r-1867qdf.r-1jgb5lz.r-pm9dpa.r-1ye8kvj.r-1rnoaur.r-13qz1uu > div > div.css-1dbjc4n.r-1awozwy.r-16y2uox > div > div.css-1dbjc4n.r-98ikmy.r-hvns9x > div.css-1dbjc4n.r-13qz1uu > div.css-18t94o4.css-1dbjc4n.r-1niwhzg.r-1ets6dv.r-sdzlij.r-1phboty.r-rs99b7.r-1wzrnnt.r-19yznuf.r-64el8z.r-1ny4l3l.r-1dye5f7.r-o7ynqc.r-6416eg.r-lrvibr').click()
+        load(driver, datas, musician)
+    # 안뜬다면 그냥
+    except TimeoutException:
+        print('실패')
+        time.sleep(3)
+        load(driver, datas, musician)
+
     return datas
 
-# datas.append({'artist': musician, 'title': title.replace('안내', ''), 'date': date, 'time': open_date[-5:], 'performance_info':f"{title}-{date}"})
+
+# getDatas('lucy')
