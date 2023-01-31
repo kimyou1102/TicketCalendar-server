bfebeb6f4279145f8b52d6d489f4ce0fb9607d55
kimyou1102 <yoosk5485@naver.com>
2023-01-31 17:46:52 +0900

    refactor: 모델 속성에 맞게 모델필드 변경

--- performanceRestAPI/models.py
+++ performanceRestAPI/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # Create your models here.
 
 class Artist(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
 
 class Ticket(models.Model):
     artist_id = models.ForeignKey(Artist, related_name="artist", on_delete=models.CASCADE, db_column="artist_id", null=True)
@@ -10,6 +10,6 @@ class Ticket(models.Model):
     date = models.CharField(max_length=200)
     date_full = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=100)
-    link = models.TextField(null=True)
+    link = models.URLField(max_length=500, null=True)
     performance_info = models.CharField(max_length=300, unique=True, null=True)
 
