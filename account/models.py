71532bb14063a347fe17fdb822adc1f931057e47
kimyou1102 <yoosk5485@naver.com>
2023-04-27 02:07:06 +0900

    refactor: p_id primarykey로 지정

--- account/models.py
+++ account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_id = models.BigIntegerField(null=True)
+    p_id = models.BigIntegerField(primary_key=True, unique=True, default=0)
     user_nickname = models.CharField(max_length=100, null=True)
     age_range = models.CharField(max_length=50, null=True)
     email = models.EmailField(max_length=100, null=True)
--- account/serializer.py
+++ account/serializer.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'user_id', 'user_nickname', 'age_range', 'email')
+        fields = ('p_id', 'user_nickname', 'age_range', 'email')
